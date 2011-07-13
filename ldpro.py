8e58d935ee241c08b50b9eeed46ab4d5907ddf7f
mhausenblas <michael.hausenblas@deri.org>
2011-07-13 17:41:33 +0100

    refactoring and some report polishing

--- ldpro.py
+++ ldpro.py
@@ -32,6 +32,11 @@ rdflib.plugin.register('sparql', rdflib.query.Processor, 'rdfextras.sparql.proce
 rdflib.plugin.register('sparql', rdflib.query.Result, 'rdfextras.sparql.query', 'SPARQLQueryResult')
 
 class LinkedDataProfiler(object):
+	
+	DEBUG = False
+	
+	WORK_DIR = 'tmp/'
+	
 	NAMESPACES = { 'void' : Namespace('http://rdfs.org/ns/void#'), 'dc' : Namespace('http://purl.org/dc/terms/') }
 
 	LDID_QUERY = """SELECT * WHERE { 
@@ -46,26 +51,32 @@ class LinkedDataProfiler(object):
 		self.g = Graph()
 		self.g.bind('void', LinkedDataProfiler.NAMESPACES['void'], True)
 		self.g.bind('dc', LinkedDataProfiler.NAMESPACES['dc'], True)
+		self.timr = { 'examples' : {}, 'sparql' : [] }
 
 	def setup(self, ldid):
-		print("Parsing [%s] for Linked Data interface description ..." %ldid)
+		if LinkedDataProfiler.DEBUG: print("Parsing [%s] for Linked Data interface description ..." %ldid)
 		self.load_ldid(ldid)
-		self.status_dump()
+		if not os.path.exists(LinkedDataProfiler.WORK_DIR):
+			os.makedirs(LinkedDataProfiler.WORK_DIR)
 
-	def profile_all(self, runs = 10):
-		for r in range(runs):
-			t = stopwatch.Timer()
-			ldpro.profile_examples()
-			t.stop()
-			print t.elapsed
-		
-	def profile_examples(self):		
+	def profile_all(self, number_of_runs = 1):
 		for ex in self.examples:
-			g = Graph()
-			print(" GETing: %s" %ex)
-			self.load_example(g, example_URI=ex)
-			file_name = ''.join([os.getcwd(), '/tmp/', ex.split('/')[-1], '.ttl'])
-			self.store_example(g, file_name)
+			runs = []
+			for r in range(number_of_runs):
+				runs.append(ldpro.profile_example(ex))
+			self.timr['examples'][ex] = runs
+
+	def profile_example(self, ex):
+		g = Graph()
+		sys.stdout.write('.')
+		sys.stdout.flush()
+		t = stopwatch.Timer()
+		self.load_example(g, example_URI=ex)
+		t.stop()
+		
+		file_name = ''.join([LinkedDataProfiler.WORK_DIR, ex.split('/')[-1], '.ttl'])
+		self.store_example(g, file_name)
+		return t
 
 	def load_example(self, g, example_URI):
 		if example_URI.endswith('.rdf'):
@@ -103,9 +114,9 @@ class LinkedDataProfiler(object):
 			except KeyError:
 				pass
 
-	def status_dump(self):
+	def check(self):
 		print('='*80 + '\n')
-		print("I'm currently working with following Linked Data interface description:\n")
+		print("I found the following Linked Data interface description:\n")
 		print(" Dataset: %s\n" %self.ds_uri)
 		print(" Title: %s\n" %self.title)
 		print(" Description: %s\n" %self.description)
@@ -114,24 +125,49 @@ class LinkedDataProfiler(object):
 		for ex in self.examples:
 			print("  + %s" %ex)
 		print('\n' + '='*80 + '\n')
+		
+	def report(self):
+		print('\n' + '='*80 + '\n')
+		print("Results for example resources:\n")
+		for ex in self.examples:
+			print(" Dereferencing %s\n" %ex)
+			i = 0
+			values = []
+			for t in self.timr['examples'][ex]:
+				print("  Run %s: %sms" %(i, t.elapsed))
+				i = i + 1
+				values.append(t.elapsed)
+			print('  ' +'-'*25)
+			average = float(sum(values)) / len(values)
+			print("  Average: %sms" %average)
+			
+			print("")
+		print('\n' + '='*80 + '\n')
 
 	def usage(self):
-		print("Usage: python ldpro.py -l {path to Linked Data interface description file} ")
+		print("Usage: python ldpro.py -l {path to Linked Data interface description file} [number of runs (optional, defaults to 3)]")
 		print("Example: python ldpro.py -l test/ldid-dbpedia.ttl")
 
 if __name__ == "__main__":
 	ldpro = LinkedDataProfiler()
 
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hl:", ["help", "ldid"])
+		opts, args = getopt.getopt(sys.argv[1:], "hl:c:", ["help", "ldid", "check"])
 		for opt, arg in opts:
 			if opt in ("-h", "--help"):
 				ldpro.usage()
 				sys.exit()
 			elif opt in ("-l", "--ldid"):
 				ldpro.setup(arg)
-				ldpro.profile_all()
-				pass
+				try:
+					number_of_runs = args[0]
+				except:
+					number_of_runs = 3 # default number of runs if not specified by user
+				ldpro.profile_all(int(number_of_runs))
+				ldpro.report()
+			elif opt in ("-c", "--check"):
+				ldpro.setup(arg)
+				ldpro.check()
 	except getopt.GetoptError, err:
 		print str(err)
 		usage()
